8d833316c87c1c7a90697795fbfde900477f90be
Joe Jarvis <joe.jarvis109@gmail.com>
2019-08-28 00:16:52 -0400

    Comments

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 let head = document.querySelector("head");
+// inject scripts into head
 ["mathjax_config.js", "MathJax/MathJax.js", "dynamic_math.js"].forEach(item => {
     let script = document.createElement("script");
     script.src = chrome.extension.getURL(item);
--- dynamic_math.js
+++ dynamic_math.js
@@ -1,24 +1,35 @@
 let $ = q => document.querySelector(q);
 
 function render_preview() {
+    // .preview is the button to switch to the preview tab
+    // .js-code-editor is the parent of both the preview content and the code editor.
+    // #readme is the preview content
+
     $(".preview").addEventListener("click", () => {
         let didLoadPreview = () => {
+            // if preview is not selected, do not load
             if (!$(".preview").classList.contains("selected")) {
                 return;
             }
+            // if preview is not loaded, wait 200ms and try again
             if (!$(".js-code-editor").classList.contains("show-preview")) {
                 setTimeout(didLoadPreview, 200);
+            } else {
+                // format #readme element when preview is loaded
+                window.MathJax.Hub.Queue(
+                    ["Typeset", window.MathJax.Hub],
+                    $("#readme")
+                );
             }
-            window.MathJax.Hub.Queue(
-                ["Typeset", window.MathJax.Hub],
-                $("#readme")
-            );
         };
         setTimeout(didLoadPreview, 200);
     });
 }
+
+// reformat when page loads dynamically
 document.addEventListener("pjax:end", () => {
     window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
     render_preview();
 });
+
 render_preview();
