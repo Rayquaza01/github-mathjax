07e0c5a93b05da49a53510372d7654d44dee6c0d
Joe Jarvis <joe.jarvis109@gmail.com>
2019-08-28 00:05:55 -0400

    Load JS w/o Jquery, fix preview

--- content.js
+++ content.js
@@ -1,10 +1,6 @@
-var config = chrome.extension.getURL('mathjax_config.js');
-var mathjax = chrome.extension.getURL('MathJax/MathJax.js');
-var jquery = chrome.extension.getURL('jquery-min-1.7.2.js');
-var dynamic_math = chrome.extension.getURL('dynamic_math.js');
-
-$.include([config, jquery], function() {
-    $.include([mathjax], function() {
-        $.include([dynamic_math]);
-    });
+let head = document.querySelector("head");
+["mathjax_config.js", "MathJax/MathJax.js", "dynamic_math.js"].forEach(item => {
+    let script = document.createElement("script");
+    script.src = chrome.extension.getURL(item);
+    head.appendChild(script);
 });
--- dynamic_math.js
+++ dynamic_math.js
@@ -1,19 +1,24 @@
+let $ = q => document.querySelector(q);
+
 function render_preview() {
-    $(".js-preview-tab").on("click", function(e) {
-        function didLoadPreview() {
-            if (!$(".js-preview-tab").hasClass('selected')) {
+    $(".preview").addEventListener("click", () => {
+        let didLoadPreview = () => {
+            if (!$(".preview").classList.contains("selected")) {
                 return;
             }
-            if ($(".preview-content").attr('display') == 'none') {
+            if (!$(".js-code-editor").classList.contains("show-preview")) {
                 setTimeout(didLoadPreview, 200);
             }
-            window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub], $(".preview-content")[0]);
-        }
+            window.MathJax.Hub.Queue(
+                ["Typeset", window.MathJax.Hub],
+                $("#readme")
+            );
+        };
         setTimeout(didLoadPreview, 200);
     });
 }
-render_preview();
-document.addEventListener("pjax:end", function(){
+document.addEventListener("pjax:end", () => {
     window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
     render_preview();
 });
+render_preview();
